c3fc05523a2b02cd9c864a7674416d6e4acfbe4a
Cory <coryborg@gmail.com>
2011-08-27 16:27:51 -0700

    A bit of refactoring, and added planets

--- server.js
+++ server.js
@@ -28,56 +28,125 @@ redis.on("error", function (err) {
     console.log( err );
 });
 
-app.get('/users.:format', function(request, response) {
-
-});
-
 //TODO: add some error checking here.
 //TODO: add validation
- 
+
+// USERS
 // Create 
+var User = {
+    get_key : function(id){ return Helper.get_key( id , 'User'); },
+    qualities : function( request ){
+                    var user_qualities = { 'email'     : request.body.email,
+                                           'location'  : request.body.location,
+                                           'speed'     : request.body.speed 
+                                          }; 
+                                          
+                    return user_qualities
+                },
+    add_or_update : function(request, response){
+        Helper.add_or_update( request, response, 'User', this.qualities( request ) )
+    }
+};
+
 app.post('/users', function(request, response) { 
-    add_or_update_user( request, response );
+    User.add_or_update( request, response );
 });
 
 // Read
 app.get('/users/:id', function(request, response) {
-  redis.get( get_user_key( request.params.id ), function( err, data ){
+  redis.get( User.get_key( request.params.id ), function( err, data ){
       response.send( JSON.stringify( data ) );
   } );
 });
 
 // Update
 app.put('/users/:id', function(request, response) {
-    add_or_update_user( request, response );
+    User.add_or_update( request, response );
 });
 
 // Delete
 app.del('/users/:id', function(request, response) {
-    redis.del( get_user_key( request.params.id ), function( err, data){
+    redis.del( User.get_key( request.params.id ), function( err, data){
         response.send( JSON.stringify( data ) );
     });
 });
 
-//TODO: perhaps abstract this a little.
-function get_user_key( id ){
-    var   type        = 'User',
-          version     = 'v1',
-          key = type + ':' + version +':' + id;
-    return key;
-}
-function add_or_update_user( request, response ){
-    var user_object = { 'email'     : request.body.email,
-                        'location'  : request.body.location,
-                        'speed'     : request.body.speed },
-        key = get_user_key( request.body.email );
-                        
-    redis.get( key, function(err, data){
-       //check error, if no error and no value returned, then insert.
-       if(!err && isNaN( parseInt( data ) ) ){
-           redis.set( key, JSON.stringify( user_object ) );
-       }
-    });
-    
-    response.send( request.body );
-}
+//PLANETS
+// Create 
+var Planet = {
+    get_key : function(id){ return Helper.get_key( id , 'Planet') },
+    qualities : function( planet ){
+                    var planet_qualities = { 'name'      : planet.name,
+                                             'location'  : planet.location,
+                                             'resources' : planet.speed,
+                                             'defense'   : planet.defense,
+                                             'owner'     : planet.owner
+                                           }; 
+                                          
+                    return planet_qualities
+                },    
+    add_or_update : function(request, response){
+        Helper.add_or_update( request, response, 'Planet', this.qualities( request ) )
+    },
+    get_planet_name : function(){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( var i=0; i < 15; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    },
+    get_planet_location : function(){
+        //not too sure about this one
+        return [0,0];
+    },
+    get_planet_resources : function(){
+        return Math.floor(Math.random()*9)
+    },
+    get_planet_defense : function(){
+        return Math.floor(Math.random()*9)
+    },
+    create_planet : function(){
+        //build planet creation logic here
+        var planet = {  'name'      : this.get_planet_name(),
+                        'location'  : this.get_planet_location(),
+                        'resource'  : this.get_planet_resources(),
+                        'defense'   : this.get_planet_defense(),
+                        'owner'     : null }
+        this.add_or_update( planet )
+    },
+    conquer_planet : function(){},
+    delete_planet : function(){},
+};
+
+// Read
+app.get('/planets/:id', function(request, response) {
+  redis.get( Planet.get_key( request.params.id ), function( err, data ){
+      response.send( JSON.stringify( data ) );
+  } );
+});
+
+//HELPERS
+
+var Helper = {
+    add_or_update : function( request, response, type, qualities ){
+        var key = this.get_key( request.body.email, type );
+
+        redis.get( key, function(err, data){
+           //check error, if no error and no value returned, then insert.
+           if(!err && isNaN( parseInt( data ) ) ){
+               redis.set( key, JSON.stringify( qualities ) );
+           }
+        });
+
+        response.send( request.body );
+    },
+    get_key : function( id, type ){
+        var   type        = type,
+              version     = 'v1',
+              key = type + ':' + version +':' + id;
+        return key;
+    }  
+};
+
